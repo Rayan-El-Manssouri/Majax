3a85c4a6e157092a3d547d13e5072af777214804
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-11-19 16:24:50 +0100

    Add ip adresse

--- api/main.py
+++ api/main.py
@@ -1,9 +1,13 @@
 from sanic import Sanic
 from sanic.response import json
+
 app = Sanic()
- 
- 
+
 @app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
-    return json({'hello': path})
+    ip_address = request.ip
+    return json({'hello': path, 'ip_address': ip_address})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
