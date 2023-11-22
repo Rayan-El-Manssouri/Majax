e010f95903de4adb4a29325115a5fe1867c30947
Rayan-El-Manssouri <liviackerman69@gmail.com>
2023-11-22 14:52:00 +0100

    Feat : change file /home with /

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
+const config = require("./config");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.use("/", home);
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+// Connection
+app.listen(config.port, (err) => {
+  if (err) {
+    console.error(`Error starting the server: ${err.message}`);
+  } else {
+    console.log(`Server is running on http://localhost:${config.port}`);
+  }
+});
